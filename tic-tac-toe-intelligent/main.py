850b1f15df23806a6a1dce4975a11ff273042a34
pavansagi <43853294+pavansagi@users.noreply.github.com>
2020-07-30 12:16:57 +0530

    Update main.py

--- tic-tac-toe-intelligent/main.py
+++ tic-tac-toe-intelligent/main.py
@@ -1,30 +1,18 @@
-import pygame
-import numpy as np
+import pygame       #this is where we play the game
+import numpy        #for 2d array
+import math         #for infinity
+import random       #for random move
+import time         #to wait for opponent after some time
+
 pygame.init()
-import math
-import time
-import sys
-# insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, r"C:\Users\david\Python")
 
-import random
-
-SIZE = WIDTH, HEIGHT = 470, 470
+SIZE = WIDTH,HEIGHT = 470,470
 
 def rect(screen, color, x, y, w, h, fill=0):
     pygame.draw.rect(screen, color, (x, y, w, h), fill)
 
 def square(screen, color, x, y, s, fill=0):
     rect(screen, color, x, y, s, s, fill)
-    
-def ellipse(screen, color, x, y, w, h, fill=0):
-    pygame.draw.ellipse(screen, color, (x, y, w, h), fill)
-
-def circle(screen, color, x, y, r, fill=0):
-    ellipse(screen, color, x, y, r, r, fill)
-
-def background(screen, color):
-    rect(screen, color, 0, 0, WIDTH, HEIGHT)
 
 def check_win(board):
     n = len(board)
@@ -74,116 +62,111 @@ def check_win(board):
         return "tie"
     return None
 
-def best_move(board):
-    n = len(board)
-    best_score = -math.inf
-    move = (0, 0)
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == "":
-                board[i][j] = "x"
-                score = minimax(board, 0, False, len(board))
-                board[i][j] = ""
-                if score > best_score:
-                    best_score = score
-                    move = (i, j)
-    board[move[0]][move[1]] = "x"
-    return board
-
 scores = {
-        "x" : 10,
-        "o" : -10,
-        "tie": 0
-    }
+    "x" : 10,
+    "o" : -10,
+    "tie" : 0
+}
 
-def minimax(board, depth, is_max, n, alpha = -math.inf, beta = math.inf):
+def minmax(board,is_max,n,alpha,beta):
     winner = check_win(board)
     if winner:
-        # print(depth)
         return scores[winner]
     if is_max:
         best_score = -math.inf
+        flag = False
         for i in range(n):
             for j in range(n):
                 if board[i][j] == "":
                     board[i][j] = "x"
-                    score = minimax(board, depth+1, False, n, alpha, beta)+random.randrange(-5, 5)
+                    score = minmax(board,False,n,alpha,beta)
                     board[i][j] = ""
-                    best_score = max(score, best_score)
-                    alpha = max(alpha, score)
-                    if beta >= alpha:
-                        pass
+                    best_score = max(best_score,score)
+                    alpha = max(alpha,score)
+                    if beta <= alpha:
+                        flag = True
+            if flag:
+                break
         return best_score
     else:
         best_score = math.inf
+        flag = False
         for i in range(n):
             for j in range(n):
                 if board[i][j] == "":
                     board[i][j] = "o"
-                    score = minimax(board, depth+1, True,n, alpha, beta)+random.randrange(-5, 5)
+                    score = minmax(board,True,n,alpha,beta)
                     board[i][j] = ""
-                    best_score = min(score, best_score)
-                    beta = min(beta, score)
-                    if alpha >= beta:
-                        pass
+                    best_score = min(best_score,score)
+                    beta = min(beta,score)
+                    if beta <= alpha:
+                        flag = True
+            if flag:
+                break
         return best_score
 
+def best_move(board):
+    n = len(board)
+    best_score = -math.inf
+    move = (0,0)
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == "":
+                board[i][j] = "x"
+                score = minmax(board,False,n,-math.inf,math.inf)
+                board[i][j] = ""
+                if score > best_score:
+                    best_score = score
+                    move = (i,j)
+    board[move[0]][move[1]] = "x"
+    return board
+
 def reset(n):
-    board = [["" for i in range(n)] for j in range(n)]
+    board = [["" for i in range(n)]for j in range(n)]
     loop = True
-    return board, loop, None, True
+    return board,loop,None,True
 
-def main():
-    padding = 10
-    n = 3
-    s = (WIDTH-padding*2)//n
     
+def main():
+    n = 3
     board = [["" for i in range(n)] for j in range(n)]
-
-    turn = "x"
+    padding = 10
+    s = (WIDTH - padding*2)//n
 
     x_image = pygame.image.load(r"ex.png")
-    x_image = pygame.transform.scale(x_image, (s, s))
+    x_image = pygame.transform.scale(x_image , (s,s));
     o_image = pygame.image.load(r"o.png")
-    o_image = pygame.transform.scale(o_image, (s, s))
+    o_image = pygame.transform.scale(o_image , (s,s));
+
 
     loop = True
     gameover = False
-
-    frame_count = 0
     human_played = False
     winner = False
     restarted = False
-    
     running = True
-    board = best_move(board)
-    turn = "x" if turn == "o" else "o"
-    # print(f"{turn}'s Turn")
-
-    screen = pygame.display.set_mode(SIZE) #Start the screen
+    turn = "o"
+    
+    screen = pygame.display.set_mode(SIZE)
 
     while running:
-        prev = frame_count
-        Mouse_x, Mouse_y = pygame.mouse.get_pos()
+        mouse_x , mouse_y = pygame.mouse.get_pos()
         for event in pygame.event.get():
-                if event.type == pygame.QUIT: #The user closed the window!
-                    running = False #Stop running
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_r:
-                        board, loop, winner, restarted = reset(n)
-                        
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    j = int(Mouse_x//s)
-                    i = int(Mouse_y//s)
-                    if i < n and j < n:
-                        if board[i][j] == "":
-                            board[i][j] = turn
-                            turn = "x" if turn == "o" else "o"
-                            # print(f"{turn}'s Turn")
-                            human_played = True
-                        winner = check_win(board)
-                        # print(np.array(board))
-                                
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    board,loop,winner,restarted = reset(n)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                i = int(mouse_y//s)
+                j = int(mouse_x//s)
+
+                if i<n and j<n:
+                    if board[i][j]=="":
+                        board[i][j] = turn
+                        turn = "x" if turn=="o" else "o"
+                        human_played = True
+                    winner = check_win(board)
 
         if loop:
             rect(screen, (255, 255, 255), padding, padding, WIDTH-padding*2, HEIGHT-padding*2)
@@ -209,23 +192,18 @@ def main():
                         screen.blit(o_image, (j*s+padding, i*s+padding))
                     square(screen, (0,0, 0), j*s+padding, i*s+padding, s, 3)
 
-
             pygame.display.update()
 
             if restarted:
-                turn = "x"
-                board = best_move(board)
-                turn = "x" if turn == "o" else "o"
+                turn = "o"
                 restarted = False
 
             if human_played:
                 time.sleep(.5)
                 board = best_move(board)
-                turn = "x" if turn == "o" else "o"
-                # print(f"{turn}'s Turn")
+                turn = "o"
                 human_played = False
-            frame_count += 1
-    pygame.quit() #Close the window
+    pygame.quit()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
